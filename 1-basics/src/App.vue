819f97f33fb4760d9d79d5db1dce23a1336c8f6d
Sergeev Dima <web.sergeev.dima@gmail.com>
2025-05-01 16:26:48 +0300

    Базовая настройка

---
+++ 1-basics/src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+const date = new Date().toLocaleDateString('ru-RU', {
+  year: "numeric",
+  month: "2-digit",
+  day: "2-digit",
+  hour: "2-digit",
+  minute: "2-digit"
+})
+</script>
+
+<template>
+  <h1>Дата: <span class="red">{{ date }}</span></h1>
+</template>
+
+<style scoped>
+.red {
+  color: red;
+}
+</style>
