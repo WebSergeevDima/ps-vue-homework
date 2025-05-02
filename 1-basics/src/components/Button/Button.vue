1a63e8489f27f2752f0dc47e6ca27e58e8537052
Sergeev Dima <web.sergeev.dima@gmail.com>
2025-05-02 12:49:41 +0300

    Внёс правки

--- 1-basics/src/components/Button/Button.vue
+++ 1-basics/src/components/Button/Button.vue
@@ -1,10 +1,17 @@
 <template>
-  <button class="button">
-    <slot />
+  <button class="button" :type="type">
+    <slot/>
   </button>
 </template>
 
 <script setup>
+const props = defineProps({
+  type:
+      {
+        type: String,
+        default: 'button',
+      },
+});
 </script>
 
 <style>
