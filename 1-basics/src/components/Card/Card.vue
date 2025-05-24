c10fd5aa3060e7104e0bb5f8c7b09719186a0e61
Дмитрий Сергеев <you@example.com>
2025-05-24 18:25:11 +0300

    Ещё раз

--- 1-basics/src/components/Card/Card.vue
+++ 1-basics/src/components/Card/Card.vue
@@ -3,15 +3,15 @@
     <div class="content">
       <div class="count">01</div>
       <div class="word">Слово</div>
-      <div class="status status--success" v-if="props.status === 'success'">
-        <IconYes />
+      <div class="status" v-if="props.status === 'success'">
+        <IconYes/>
       </div>
-      <div class="status status--fail" v-if="props.status === 'fail'">
-        <IconNo />
+      <div class="status" v-if="props.status === 'fail'">
+        <IconNo/>
       </div>
       <div class="actionBtns" v-if="props.isTurnCard && props.status === 'pending'">
-        <IconNo @click="handleAction(false)" class="btn" />
-        <IconYes @click="handleAction(true)" class="btn" />
+        <IconNo @click="handleAction(false)" class="btn"/>
+        <IconYes @click="handleAction(true)" class="btn"/>
       </div>
       <div
           class="actionTurn"
@@ -23,10 +23,11 @@
     </div>
   </div>
 </template>
+
 <script setup>
 import IconYes from "../../icons/IconYes.vue";
 import IconNo from "../../icons/IconNo.vue";
-import { defineProps, defineEmits } from "vue";
+import {defineProps, defineEmits} from "vue";
 
 const props = defineProps({
   isTurnCard: {
@@ -59,6 +60,7 @@ const handleAction = (isAnswer) => {
   emit('addAction', isAnswer);
 };
 </script>
+
 <style scoped>
 .card {
   width: 250px;
@@ -67,12 +69,14 @@ const handleAction = (isAnswer) => {
   box-shadow: 0 0 16px 0 #0000001A;
   border-radius: 16px;
 }
+
 .content {
   border: 1px var(--bg-primary) solid;
   border-radius: 12px;
   position: relative;
   height: 100%;
 }
+
 .count {
   font-size: 14px;
   font-weight: 400;
@@ -83,6 +87,7 @@ const handleAction = (isAnswer) => {
   padding: 0 3px;
   z-index: 1;
 }
+
 .word {
   position: absolute;
   top: 50%;
@@ -91,6 +96,7 @@ const handleAction = (isAnswer) => {
   font-weight: 400;
   font-size: 18px;
 }
+
 .status {
   position: absolute;
   top: -8px;
@@ -99,13 +105,12 @@ const handleAction = (isAnswer) => {
   width: 40px;
   z-index: 1;
 }
+
+.btn {
+  cursor: pointer;
+}
+
 .actionBtns {
-  text-transform: uppercase;
-  font-size: 12px;
-  font-weight: 700;
-  position: absolute;
-  bottom: -20px;
-  left: 50%;
   transform: translate(-50%, -50%);
   background-color: var(--color-white);
   padding: 0 5px;
@@ -113,21 +118,25 @@ const handleAction = (isAnswer) => {
   display: flex;
   justify-content: space-between;
   z-index: 1;
+  text-transform: uppercase;
+  font-size: 12px;
+  font-weight: 700;
+  position: absolute;
+  bottom: -20px;
+  left: 50%;
 }
+
 .actionTurn {
+  transform: translate(-50%, -50%);
+  background-color: var(--color-white);
+  padding: 0 5px;
+  min-width: 85px;
+  z-index: 1;
   text-transform: uppercase;
   font-size: 12px;
   font-weight: 700;
   position: absolute;
   bottom: -14px;
   left: 50%;
-  transform: translate(-50%, -50%);
-  background-color: var(--color-white);
-  padding: 0 5px;
-  min-width: 85px;
-  z-index: 1;
-}
-.btn {
-  cursor: pointer;
 }
 </style>
